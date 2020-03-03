10676485a4cbf7bf0eb51eb179865e21983bc1b1
Javier García <javigdeveloper@gmail.com>
2020-03-03 14:48:03 -0500

    some-styling

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import './Game.css'
 
 function Game() {
   let colors = ["yellow", "blue", "red", "black"]
   let sample = colors[0]
 
   return (
-    <div>
-      <p>the sky is {sample}</p>
-      <button>{colors[1]}</button>
-      <button>{colors[2]}</button>
+    <div className="game-box">
+      <div className="text-box">
+        <p>the sky is {sample}</p>
+      </div>
+      <div className="b1">
+        <button>{colors[1]}</button>
+      </div>
+      <div className="b2">
+        <button>{colors[2]}</button>
+      </div>
     </div>
   )
 }
