1a5f0153542b45d5467c9d74350f631536ee1b93
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-03-03 16:00:21 -0500

    updated game class

--- src/Game.js
+++ src/Game.js
@@ -4,19 +4,41 @@ class Game extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      phrase: this.changePhrase()
+      phrase: this.randomPhrase(),
+      word: this.randomWord(),
+      color: this.randomWordColor()
     }
   }
 
-  changePhrase(){
+  randomPhrase(){
     const phrases = ["The car is", "Today, the sky was", "My favorite color is", "It looks like that orange is actually", "The painter used a lot of"]
     return phrases[Math.floor(Math.random() * phrases.length)]
+  }
+
+  randomWord(){
+    const colors = ["red", "yellow", "blue", "green", "orange", "purple", "pink", "black", "brown", "gray"]
+    return colors[Math.floor(Math.random() * colors.length)]
+  }
+
+  randomWordColor(){
+    const fontColor = ["red", "blue", "green"]
+    let changeFont = fontColor[Math.floor(Math.random() * fontColor.length)]
+    return changeFont;
+  }
+
+  changeEverything(){
+    this.setState({
+      phrase: this.randomPhrase(),
+      word: this.randomWord(),
+      color: this.randomWordColor()
+    })
   }
 
   render() {
     return (
       <div>
-        <p>{this.state.phrase}</p>
+        <button onClick={() => this.changeEverything()}>next</button>
+        <p className="phrase">{this.state.phrase} <span className={this.state.color}>{this.state.word}</span></p>
       </div>
     )
   }
